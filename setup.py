b3225c1b2078f4c9316b403e4359ad84a1979951
James Larsen <James.Larsen42@gmail.com>
2023-05-02 07:31:21 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   .gitignore
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexus-utilities',
-    version='0.1.5',
+    version='0.1.6',
     author='James Larsen',
     author_email='james.larsen42@gmail.com',
     description='Common python utilities',
     long_description='This package is meant to hold various useful utilities for functionality I find myself using across multiple projects.  I will try to keep this documentation updated as I expand the toolkit.',
     long_description_content_type='text/markdown',
-    url='https://github.com/james-larsen/nexus_utils',
+    url='https://github.com/james-larsen/nexus-utilities',
     packages=find_packages(),
     install_requires=[
         'boto3>=1.26.45',
