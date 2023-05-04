b2b813aacef63cbb3f665f5adcc508c2c24cc2ae
James Larsen <James.Larsen42@gmail.com>
2023-05-04 14:41:20 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexus-utilities',
-    version='0.1.9',
+    version='0.1.10',
     author='James Larsen',
     author_email='james.larsen42@gmail.com',
     description='Common python utilities',
