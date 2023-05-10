cce3e540265aa84887fb83e37907355367896589
James Larsen <James.Larsen42@gmail.com>
2023-05-10 08:45:57 -0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   nexus_utils/config_utils.py
    	modified:   setup.py

--- nexus_utils/config_utils.py
+++ nexus_utils/config_utils.py
@@ -1,18 +1,21 @@
 """Configuration-related utilities"""
 #%%
 import os
-import glob
+import pathlib
+# import glob
 import configparser
-import io
+# import io
 
 #%%
 def read_config_file(config_filepath):
     """Read config .ini file at provided location, using '_local' version instead if found"""
+    if isinstance(config_filepath, str):
+        config_filepath = pathlib.Path(config_filepath)
     config = configparser.ConfigParser()
     break_out_flag = False
     # Locate local config file
     local_config_path = ''
-    for root, dirs, files in os.walk(os.getcwd()):
+    for root, dirs, files in os.walk(config_filepath.parent):
         if break_out_flag == True:
             break
         files = [file for file in files if file.endswith(('.ini'))]
@@ -20,11 +23,10 @@ def read_config_file(config_filepath):
             if break_out_flag == True:
                 break
             if file == config_filepath.stem + '_local.ini':
-                local_config_path = os.path.join(root, file)
+                # local_config_path = os.path.join(root, file)
+                local_config_path = pathlib.Path(root) / file
                 break_out_flag = True
                 break
-                #fp_local = io.open(local_config_path, 'r', encoding='utf_8_sig')
-    #fp = io.open(config_filepath, 'r', encoding='utf_8_sig')
     config.read([config_filepath, local_config_path])#, encoding='utf-8-sig')
     return config
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nexus-utilities',
-    version='0.2.6',
+    version='0.2.7',
     author='James Larsen',
     author_email='james.larsen42@gmail.com',
     description='Common python utilities',
